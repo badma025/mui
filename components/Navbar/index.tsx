52c50ca40e6f9ddf88581e0ddcc88bf9c346fca5
turbofuture1 <alimbadmus239@gmail.com>
2022-01-16 10:49:03 +0000

    updated on 16 January 2022

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,9 +1,45 @@
 import { NextPage } from "next";
 import { makeStyles } from "@mui/styles";
 import { styleTheme } from "../../pages/theme";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import {
+  alpha,
+  AppBar,
+  Avatar,
+  Badge,
+  InputBase,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { Mail, Notifications, Search } from "@mui/icons-material";
+import { useState } from "react";
 
 const useStyles = makeStyles((theme: any) => ({
+  searchIcon: {
+    marginLeft: theme.spacing(1),
+  },
+  search: {
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    borderRadius: theme.shape.borderRadius,
+    width: "50%",
+    [theme.breakpoints.down("sm")]: {
+      // @ts-ignore
+      display: (props) => (props.open ? "flex" : "none"),
+    },
+  },
+  input: {
+    color: "white",
+    marginLeft: theme.spacing(1),
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
   logoLg: {
     display: "none",
     // if it's bigger than sm we show
@@ -22,20 +58,57 @@ const useStyles = makeStyles((theme: any) => ({
     // @ts-ignore
     ...styleTheme.themeColor,
   },
+  icons: {
+    alignItems: "center",
+    // @ts-ignore
+    display: (props) => (props.open ? "none" : "flex"),
+  },
+  badge: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+  },
+  searchButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
 }));
 
 const Navbar: NextPage = () => {
-  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  // this is a prop variable
+  const classes = useStyles({ open });
 
   return (
     <AppBar className={classes.theme}>
-      <Toolbar>
+      <Toolbar className={classes.toolbar}>
         <Typography variant="h6" className={classes.logoLg}>
           turbofuture1
         </Typography>
         <Typography variant="h6" className={classes.logoSm}>
           turbo
         </Typography>
+        <div className={classes.search}>
+          <SearchIcon className={classes.searchIcon} />
+          <InputBase placeholder="Search..." className={classes.input} />
+        </div>
+        <div className={classes.icons}>
+          <Search
+            className={classes.searchButton}
+            onClick={() => setOpen(!open)}
+          />
+          <Badge badgeContent={4} color="secondary">
+            <Mail />
+          </Badge>
+          <Badge className={classes.badge} badgeContent={2} color="secondary">
+            <Notifications />
+          </Badge>
+          <Avatar
+            alt="Alim Badmus"
+            src="https://images.pexels.com/photos/8647814/pexels-photo-8647814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          />
+        </div>
       </Toolbar>
     </AppBar>
   );
